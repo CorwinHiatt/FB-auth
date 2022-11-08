4a1a5d4574fa7064d0430eddda62427ba8ea78c2
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-08 11:37:30 -0500

    added signin with email and password

---
+++ src/scenes/Login.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import { initializeApp } from "firebase/app"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjclId9AjJ1EUKC8T0Evpg_-a29UjRLt0",
+    authDomain: "fb-auth-cj.firebaseapp.com",
+    projectId: "fb-auth-cj",
+    storageBucket: "fb-auth-cj.appspot.com",
+    messagingSenderId: "437335609197",
+    appId: "1:437335609197:web:27ca8f6ece801f1e6e5464"
+};
+
+export default function Login({setUser}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        const app = initializeApp(firebaseConfig) //this connects to firebase 
+        const auth = getAuth(app) // connects us to firebase auth
+        const response = await signInWithEmailAndPassword(auth, email, password) //sign us in 
+            .catch(alert)
+       setUser(response.user)
+    }
+    return (
+        <>
+
+            <h1>LOGIN</h1>
+
+            <form onSubmit={handleLogin}>
+                <label htmlFor="email">Email:{''}
+                    <input type='email' name='email'
+                        value={email} onChange={e => setEmail(e.target.value)}
+                        placeholder="yourname@domain.com" />
+                </label><br />
+                <label htmlFor="Password">Password:{''}
+                    <input type='Password' name='Password'
+                        value={password} onChange={e => setPassword(e.target.value)}
+                        placeholder="*****" />
+                </label><br />
+                <button type="submit">Login</button>
+            </form>
+        </>
+    )
+}
---
+++ src/scenes/Protected.jsx
@@ -0,0 +1,11 @@
+export default function Protected() {
+    return(
+       <>
+        <h1>
+        protected
+        </h1>
+        <p>Welcome user this is the hottestest sight around</p>
+
+        </>
+    )
+}
